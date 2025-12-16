f0e7d23dc6c69cc86245ee515fd7cd4aa323feed
josefgst <josefgstoettner@live.at>
2025-12-16 16:18:13 +0800

    add PID logging

--- hardware/diffbot_system.cpp
+++ hardware/diffbot_system.cpp
@@ -154,6 +154,7 @@ hardware_interface::CallbackReturn DiffDriveArduinoHardware::on_activate(
   if (cfg_.pid_p > 0)
   {
     comms_.set_pid_values(cfg_.pid_p,cfg_.pid_d,cfg_.pid_i,cfg_.pid_o);
+    RCLCPP_INFO(get_logger(), "PID values set: P=%f, D=%f, I=%f, O=%f", cfg_.pid_p, cfg_.pid_d, cfg_.pid_i, cfg_.pid_o);
   }
   RCLCPP_INFO(get_logger(), "Successfully activated!");
   //END: Custom code
